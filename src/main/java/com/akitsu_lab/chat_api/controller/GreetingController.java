f9b176bd1e0760edc632976eb85fb8135cbc3ead
nagaokakeita <nagakeita12@gmail.com>
2024-08-16 01:19:40 +0900

    Use JdbcClient which was new in Spring boot 3.2

--- src/main/java/com/akitsu_lab/chat_api/controller/GreetingController.java
+++ src/main/java/com/akitsu_lab/chat_api/controller/GreetingController.java
@@ -1,7 +1,7 @@
 package com.akitsu_lab.chat_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +12,13 @@ import java.util.Map;
 public class GreetingController {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private JdbcClient jdbcClient;
 
     @GetMapping("/greeting")
     public String greeting (){
 
-        // reference: https://dev.classmethod.jp/articles/use_spring-boot_jdbctemplate/
-        // executeはCREATEとかDROPの時に使う
-        jdbcTemplate.execute("SELECT * FROM users");
-
-        // SELECT文を使う時はこんな感じ
-        List<Map<String, Object>> list = jdbcTemplate.queryForList("SELECT * FROM users");
-        list.forEach(System.out::println);
+        // JdbcTemplateじゃなくてJdbcClientを使う方がスマートかも
+        jdbcClient.sql("SELECT * FROM users").query().listOfRows().forEach(System.out::println);
         return "Hello";
     }
 }
